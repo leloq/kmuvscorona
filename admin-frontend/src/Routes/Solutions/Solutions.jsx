9e80bd7c2f3e3fea275fd4d0a58da2e9da0bba6a
pjckoch <patrick.koch1994@gmail.com>
2020-03-21 17:22:33 +0100

    Enable creating of solutions in admin frontend

--- admin-frontend/src/Routes/Solutions/Solutions.jsx
+++ admin-frontend/src/Routes/Solutions/Solutions.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { makeStyles, Fab } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
 import { useDispatch } from 'react-redux';
+import { navigate } from '@reach/router';
 import SolutionsTable from './SolutionsTable';
+
+const useStyles = makeStyles(theme => ({
+    fab: {
+        margin: 0,
+        position: 'fixed',
+        top: 'auto',
+        bottom: theme.spacing(5),
+        left: 'auto',
+        right: theme.spacing(5),
+    }
+}));
 
 const Solutions = () => {
     const dispatch = useDispatch();
+    const classes = useStyles();
+
+    const navigateToNewSolutionForm = () => {
+        navigate('/newSolution');
+    };
+
     dispatch({
         type: 'Solutions/getSolutions',
     });
-    return <SolutionsTable />;
+    return (
+        <>
+            <SolutionsTable />
+            <Fab onClick={navigateToNewSolutionForm} className={classes.fab} color="primary">
+                <Add />
+            </Fab>
+        </>
+    );
 };
 
 export default Solutions;
