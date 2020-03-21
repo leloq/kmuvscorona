f381df2f677121b64e231035db09b04d0bbeb90e
Leo Semmelmann <56198292+leloq@users.noreply.github.com>
2020-03-21 20:08:36 +0100

    style reworked

--- user-frontend/src/components/SolutionsOverview.js
+++ user-frontend/src/components/SolutionsOverview.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import SolutionsList from './SolutionsList'
 import Grid from '@material-ui/core/Grid';
 import axios from './../axiosInstance';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 export default class SolutionsOverview extends Component {
 
@@ -23,8 +25,13 @@ export default class SolutionsOverview extends Component {
 
   render() {
     return (
-      <div>
-          <SolutionsList solutions={this.state.solutions}/>        
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+      <Container maxWidth="sm" class="hero" >
+          <Typography variant="h3" align="center" className="lg-mg-bottom">
+          Alle unsere Lösungsansätze
+        </Typography>
+          <SolutionsList solutions={this.state.solutions}/>  
+        </Container>      
       </div>
     )
   }
