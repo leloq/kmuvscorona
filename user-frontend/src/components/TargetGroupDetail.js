6246f161cdee222f5c39107081fc3fa758a80f25
Lennart Lehmann <lennart.lehmann@tum.de>
2020-03-30 13:02:21 +0200

    Added the sharing functionality for each target group

--- user-frontend/src/components/TargetGroupDetail.js
+++ user-frontend/src/components/TargetGroupDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from './../axiosInstance';
 import { withStyles, Grid, Typography } from '@material-ui/core/';
 import ProblemList from './ProblemList';
+import Button from '@material-ui/core/Button';
+import {InlineShareButtons} from 'sharethis-reactjs';
 
 const styles = {
   root: {
@@ -90,6 +92,40 @@ class TargetGroupDetail extends Component {
       <div className={this.props.classes.root}>
          { this.descriptionHeader() }
          <div><ProblemList targetgroupid={this.props.match.params.id} problems={this.state.problems}/>  </div>
+          
+          <div> 
+            <InlineShareButtons
+              config={{
+                alignment: 'center',  // alignment of buttons (left, center, right)
+                color: 'social',      // set the color of buttons (social, white)
+                enabled: true,        // show/hide buttons (true, false)
+                font_size: 16,        // font size for the buttons
+                labels: 'cta',        // button labels (cta, counts, null)
+                language: 'en',       // which language to use (see LANGUAGES)
+                networks: [           // which networks to include (see SHARING NETWORKS)
+                  'whatsapp',
+                  'linkedin',
+                  'facebook',
+                  'twitter',
+                  'email'
+                ],
+                padding: 12,          // padding within buttons (INTEGER)
+                radius: 4,            // the corner radius on each button (INTEGER)
+                show_total: true,
+                size: 40,             // the size of each button (INTEGER)
+
+                // OPTIONAL PARAMETERS
+                // url: 'https://www.sharethis.com', // (defaults to current url)
+                // image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+                // description: 'custom text',       // (defaults to og:description or twitter:description)
+                // title: 'custom title',            // (defaults to og:title or twitter:title)
+                // message: 'custom email text',     // (only for email sharing)
+                //subject: 'custom email subject',  // (only for email sharing)
+                // username: 'custom twitter handle' // (only for twitter sharing)
+              }}
+            />
+          </div>
+
       </div>
     );
   }
