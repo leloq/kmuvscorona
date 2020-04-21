dd01170d8db6b95515cab4f6332b86a0fbb35567
Armin Satzger <armin.satzger@gmail.com>
2020-04-21 18:18:58 +0200

    Add explanatory text solutions overview

--- user-frontend/src/components/SolutionsOverview.js
+++ user-frontend/src/components/SolutionsOverview.js
@@ -3,6 +3,8 @@ import SolutionsList from './SolutionsList'
 import axios from './../axiosInstance';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
 
 export default class SolutionsOverview extends Component {
 
@@ -26,9 +28,14 @@ export default class SolutionsOverview extends Component {
     return (
       <div style={{display: 'flex', justifyContent: 'center'}}>
       <Container maxWidth="sm" class="hero" >
-          <Typography variant="h3" align="center" className="lg-mg-bottom">
-          Alle unsere Lösungsansätze
-        </Typography>
+          <Typography variant="h3" align="center" className="lg-mg-bottom" gutterBottom>
+            All unsere Lösungsansätze
+          </Typography>
+
+          <Typography gutterBottom>
+            Auf dieser Seite finden Sie alle Lösungsvorschläge unabhängig von den relevanten Zielgruppen und Lösungen zusammengefasst. In die Detailansicht der für Ihre Zielgruppe relevanten Lösungsansätze gelangen Sie durch das Klicken auf den entsprechenden Button auf der Startseite.
+          </Typography>
+
           <SolutionsList solutions={this.state.solutions}/>  
         </Container>      
       </div>
