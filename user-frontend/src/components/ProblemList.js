3d9e930d39bfd59dbc830e8d32cccdd154a168a3
Patrick Koch <pjc.koch@gmail.com>
2020-04-26 13:19:46 +0200

    Sort problems by severity

--- user-frontend/src/components/ProblemList.js
+++ user-frontend/src/components/ProblemList.js
@@ -2,6 +2,11 @@ import React  from 'react';
 import ProblemDetail from './ProblemDetail'
 
 export default function ProblemList(props){
+
+  props.problems.sort(function(prob1, prob2){ // in-place sorting
+    return (prob1.severity - prob2.severity); // sort ascending: highest severity (smallest number) first
+  });
+
 const problemList = () => {
     return props.problems.map(problem => {
       return( 
