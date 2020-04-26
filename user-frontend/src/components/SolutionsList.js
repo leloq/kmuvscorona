a5f6a0df5f394ade4b7a6b9b3b8dc05eb969007c
Patrick Koch <pjc.koch@gmail.com>
2020-04-26 13:09:57 +0200

    Sort solutions by popularity

--- user-frontend/src/components/SolutionsList.js
+++ user-frontend/src/components/SolutionsList.js
@@ -2,10 +2,17 @@ import React from 'react';
 import SolutionsListPanel from './SolutionsListPanel'
 import Grid from '@material-ui/core/Grid';
 
+
 export default function SolutionsList(props){
 
+const sortedSolutions = props.solutions.slice().sort(function(sol1, sol2){ // use slice() to sort COPY of array
+  var sol1VoteBalance = sol1.upVotes - sol1.downVotes;
+  var sol2VoteBalance = sol2.upVotes - sol2.downVotes;
+  return (sol2VoteBalance - sol1VoteBalance); // sort descending: most popular first
+});
+
 const solutionsList = () => {
-    return props.solutions.map(solution => {
+    return sortedSolutions.map(solution => {
       return( 
         <Grid item key={solution._id}>
           <SolutionsListPanel solution={solution}/>
