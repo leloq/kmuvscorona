31a99633fee03defe3c8acef7cbb84a96ad71056
Patrick Koch <pjc.koch@gmail.com>
2020-04-26 13:12:55 +0200

    Switch to in-place sorting

--- user-frontend/src/components/SolutionsList.js
+++ user-frontend/src/components/SolutionsList.js
@@ -5,14 +5,14 @@ import Grid from '@material-ui/core/Grid';
 
 export default function SolutionsList(props){
 
-const sortedSolutions = props.solutions.slice().sort(function(sol1, sol2){ // use slice() to sort COPY of array
-  var sol1VoteBalance = sol1.upVotes - sol1.downVotes;
-  var sol2VoteBalance = sol2.upVotes - sol2.downVotes;
-  return (sol2VoteBalance - sol1VoteBalance); // sort descending: most popular first
-});
+  props.solutions.sort(function(sol1, sol2){ // in-place sorting
+    var sol1VoteBalance = sol1.upVotes - sol1.downVotes;
+    var sol2VoteBalance = sol2.upVotes - sol2.downVotes;
+    return (sol2VoteBalance - sol1VoteBalance); // sort descending: most popular first
+  });
 
 const solutionsList = () => {
-    return sortedSolutions.map(solution => {
+    return props.solutions.map(solution => {
       return( 
         <Grid item key={solution._id}>
           <SolutionsListPanel solution={solution}/>
